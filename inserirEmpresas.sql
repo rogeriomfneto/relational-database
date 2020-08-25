9a818866afdaf7e8232a5f078ac4476f1979c512
Rogerio Neto <rogerioneto@hotmail.com>
2020-08-24 21:02:11 -0300

    insert values into tables empresas and empresas_unidades

---
+++ inserirEmpresas.sql
@@ -0,0 +1,21 @@
+alter table empresas modify cnpj varchar(14);
+
+insert into empresas
+  (nome, cnpj)
+values 
+  ('Bradesco', 95694186000132),
+  ('Vale', 27887148000146),
+  ('Cielo', 01598317000134);
+
+desc empresas;
+desc prefeitos;
+select * from empresas;
+select * from cidades;
+
+insert into empresas_unidades
+  (empresa_id, cidade_id, sede)
+values
+  (1, 1, 1),
+  (1, 2, 0),
+  (2, 1, 0),
+  (2, 2, 1);
